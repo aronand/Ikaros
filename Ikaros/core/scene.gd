2f15f5a7454ac9a1b768efe974670252f6d82b8f
aronand <71503746+aronand@users.noreply.github.com>
2024-11-23 16:06:13 +0200

    feat: Attach the camera dynamically to the player
    * Camera3D removed from the Scene
    * CameraPosition node added to Character
    * Camera3D created and attached to CameraPosition when Scene is ready and a player is present

--- Ikaros/core/scene.gd
+++ Ikaros/core/scene.gd
@@ -12,3 +12,17 @@ func _init() -> void:
 func _ready() -> void:
 	if player == null:
 		printerr("Player not defined in scene")
+	else:
+		attach_camera_to_player()
+
+
+func attach_camera_to_player() -> void:
+	# Attach a camera to the player
+	var camera: Camera3D = Camera3D.new()
+	camera.current = true
+	player.add_child(camera)
+
+	# Move and rotate camera to proper position
+	var camera_position: Node3D = player.find_child("CameraPosition")
+	camera.position = camera_position.position
+	camera.rotation_degrees.y = 180
