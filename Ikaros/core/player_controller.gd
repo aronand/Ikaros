7bd6475d3ec4424e11c2ece00687a81c27f8a9a8
aronand <71503746+aronand@users.noreply.github.com>
2024-11-23 15:46:20 +0200

    refactor: Limit direction length instead of normalizing
    Input strength from a joystick is taken into account when direction vector length is limited instead of normalized.

--- Ikaros/core/player_controller.gd
+++ Ikaros/core/player_controller.gd
@@ -10,7 +10,7 @@ var _move_command := IkarosCharacterMoveCommand.new()
 
 func _process(delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction: Vector3 = (_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction: Vector3 = (_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).limit_length()
 	if direction:
 		var move_params := IkarosCharacterMoveCommand.Params.new()
 		move_params.direction = direction
