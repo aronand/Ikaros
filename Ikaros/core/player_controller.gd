b3677d2a15772e451cc2709cd67ee6432559830a
aronand <71503746+aronand@users.noreply.github.com>
2024-12-02 16:08:37 +0200

    refactor: Turns player direction vector into a private member

--- Ikaros/core/player_controller.gd
+++ Ikaros/core/player_controller.gd
@@ -2,6 +2,7 @@ class_name IkarosPlayerController
 extends Node
 
 var _player: IkarosCharacter
+var _direction: Vector3
 
 var _jump_command: IkarosCharacterJumpCommand
 var _move_command: IkarosCharacterMoveCommand
@@ -22,16 +23,16 @@ func _process(_delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector(
 		"move_right", "move_left", "move_backward", "move_forward"
 	)
-	var direction: Vector3
-	direction = (_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).limit_length()
-	if direction:
+
+	_direction = (_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).limit_length()
+	if _direction:
 		# HACK: Rotate the player character's collision shape to match with camera node.
 		# This will rotate the mesh as well. Cursed af, but works for now.
 		var camera_root: Node3D = _player.find_child("CameraRoot")
 		var col_shape: CollisionShape3D = _player.find_child("CollisionShape3D")
 		col_shape.rotation.y = camera_root.rotation.y
 
-		var relative_dir: Vector3 = direction.rotated(Vector3.UP, camera_root.rotation.y)
+		var relative_dir: Vector3 = _direction.rotated(Vector3.UP, camera_root.rotation.y)
 		_move_params.direction = relative_dir
 		_move_command.execute(_player, _move_params)
 
