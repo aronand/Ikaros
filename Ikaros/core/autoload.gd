29ccade9ff019af52807c22fec43d249d3414e5e
aronand <71503746+aronand@users.noreply.github.com>
2024-12-02 20:31:03 +0200

    feat: Adds gravity to Ikaros autoload

--- Ikaros/core/autoload.gd
+++ Ikaros/core/autoload.gd
@@ -3,6 +3,8 @@ extends Node
 
 var log_level: LogStream.LogLevel = LogStream.LogLevel.DEBUG
 
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func get_logger(log_name: String) -> LogStream:
 	return LogStream.new(log_name, log_level)
