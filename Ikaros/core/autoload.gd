69d54b27985b1752977e9a2ec693fe6e51d013be
aronand <71503746+aronand@users.noreply.github.com>
2024-12-04 20:52:37 +0200

    feat: Adds a reference to the player in the Ikaros autoload

--- Ikaros/core/autoload.gd
+++ Ikaros/core/autoload.gd
@@ -5,6 +5,8 @@ var log_level: LogStream.LogLevel = LogStream.LogLevel.DEBUG
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var player: IkarosCharacter = null
+
 
 func get_logger(log_name: String) -> LogStream:
 	return LogStream.new(log_name, log_level)
