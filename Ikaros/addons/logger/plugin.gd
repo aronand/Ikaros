956488e378adbaaeb4f7261ab4359baec6755851
aronand <71503746+aronand@users.noreply.github.com>
2024-12-11 21:51:56 +0200

    chore: gdformat on addons/logger

--- Ikaros/addons/logger/plugin.gd
+++ Ikaros/addons/logger/plugin.gd
@@ -2,10 +2,7 @@
 extends EditorPlugin
 
 
-
 func _enable_plugin():
-	
-	
 	#make sure log-stream is loaded to prevent godot error.
 	preload("res://addons/logger/log-stream.gd")
 	add_autoload_singleton("Log", "res://addons/logger/logger.gd")
--- Ikaros/addons/logger/settings.gd
+++ Ikaros/addons/logger/settings.gd
@@ -9,11 +9,10 @@ const LOG_MESSAGE_FORMAT_DEFAULT_VALUE = "{log_name}/{level} [lb]{hour}:{minute}
 const USE_UTC_TIME_FORMAT_KEY = "addons/Log/use_utc_time_format"
 const USE_UTC_TIME_FORMAT_DEFAULT_VALUE = false
 
-## Enables a breakpoint to mimic the godot behavior where the application doesn't crash when connected to debug environment, 
+## Enables a breakpoint to mimic the godot behavior where the application doesn't crash when connected to debug environment,
 ## but instead freezed and shows the stack etc in the debug panel.
 const BREAK_ON_ERROR_KEY = "addons/Log/break_on_error"
 const BREAK_ON_ERROR_DEFAULT_VALUE = true
-
 
 ##Whether to dump the tree to the log on error.
 const PRINT_TREE_ON_ERROR_KEY = "addons/Log/print_tree_on_error"
