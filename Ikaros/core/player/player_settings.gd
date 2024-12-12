316f326f20b799bf60f05d74554c7494788aa425
aronand <71503746+aronand@users.noreply.github.com>
2024-12-12 19:32:43 +0200

    feat: Adds PlayerSettings.face_movement_direction

--- Ikaros/core/player/player_settings.gd
+++ Ikaros/core/player/player_settings.gd
@@ -1,4 +1,8 @@
 class_name IkarosPlayerSettings
 extends Resource
 
+## If true, player has full control over movement while in the air.
 @export var can_move_in_air: bool = true
+
+## If true, player character will rotate towards movement direction when in third person mode.
+@export var face_movement_direction: bool = true
