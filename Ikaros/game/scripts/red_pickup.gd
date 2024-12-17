b8f6020dc7eaeedfa4d6114c4c53d14d17dc7617
aronand <71503746+aronand@users.noreply.github.com>
2024-12-17 20:55:06 +0200

    feat: Adds behavioural flags to red_pickup

--- Ikaros/game/scripts/red_pickup.gd
+++ Ikaros/game/scripts/red_pickup.gd
@@ -1,5 +1,16 @@
 extends Area3D
 
+## A persistent pickup will not get removed upon interaction.
+@export var is_persistent: bool = false
+
+## A magnetic pickup will float towards the player when close enough.
+@export var is_magnetic: bool = false  # TODO: Implement
+
+## A pickup that requires input behaves like an interactable object,
+## with the difference that it requires the player to have also entered
+## the pickup's collision shape.
+@export var requires_input: bool = false  # TODO: Implement
+
 @onready var _col_shape: CollisionShape3D = find_child("CollisionShape3D") as CollisionShape3D
 
 
@@ -12,6 +23,12 @@ func _process(_delta: float) -> void:
 	_col_shape.rotation.x = 0.0
 
 
+func _physics_process(_delta: float) -> void:
+	if not is_magnetic:
+		return
+	# TODO: Implement magnetic pickup behaviour
+
+
 func _on_body_entered(body: Node3D) -> void:
 	if body is not IkarosCharacter:
 		return
@@ -20,4 +37,5 @@ func _on_body_entered(body: Node3D) -> void:
 		return
 
 	Ikaros.score += 1
-	queue_free()
+	if not is_persistent:
+		queue_free()
