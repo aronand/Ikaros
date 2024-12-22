e485e528322ba26b7416287e0ad0a7512eb161d2
aronand <71503746+aronand@users.noreply.github.com>
2024-12-22 10:27:08 +0200

    refactor: Don't recreate variables each process loop in state label

--- Ikaros/game/scripts/state_label.gd
+++ Ikaros/game/scripts/state_label.gd
@@ -1,8 +1,15 @@
 extends Label
 
 
-func _process(_delta: float) -> void:
+var state_machine: IkarosStateMachine
+var state: IkarosCharacterState
+
+
+func _ready() -> void:
 	var parent: IkarosScene = owner as IkarosScene
-	var state_machine: IkarosStateMachine = parent.player.find_child("IkarosStateMachine")
-	var state: IkarosCharacterState = state_machine.state
+	state_machine = parent.player.find_child("IkarosStateMachine")
+
+
+func _process(_delta: float) -> void:
+	state = state_machine.state
 	text = "State: %s" % state.name
