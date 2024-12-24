75b20d40b6e62daeb828f63f79a5ee2e58f103f3
aronand <71503746+aronand@users.noreply.github.com>
2024-12-24 10:06:11 +0200

    feat: Adds script to verify correct engine version is used

---
+++ Ikaros/core/build/verify_engine_version.gd
@@ -0,0 +1,19 @@
+extends SceneTree  # We can't extend MainLoop as that lacks the quit() method
+
+var exit_code: int = 0
+
+
+func _initialize() -> void:
+	print("Project engine version: %s" % Ikaros.project_engine_version)
+	print("Engine version: %s" % Ikaros.engine_version)
+
+	if Ikaros.is_correct_engine_version():
+		print("Engine version matches project's engine version.")
+		exit_code = 0
+	else:
+		printerr("Engine version does not match project's engine version.")
+		exit_code = 1
+
+
+func _finalize() -> void:
+	quit(exit_code)
