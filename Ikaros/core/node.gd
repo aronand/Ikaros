4b3b188bd8d7915b52daeef1a22cd7bc87a19c18
aronand <71503746+aronand@users.noreply.github.com>
2024-12-24 13:46:33 +0200

    feat: Adds logging functions to IkarosNode

--- Ikaros/core/node.gd
+++ Ikaros/core/node.gd
@@ -2,6 +2,8 @@ class_name IkarosNode
 extends Node
 ## Base class for Node based Ikaros nodes.
 
+@onready var _logger: LogStream = Ikaros.get_logger(get_class_name())
+
 
 ## Returns the class name as defined in the script, or an empty string if no
 ## script is attached.
@@ -10,3 +12,28 @@ func get_class_name() -> StringName:
 	if script == null:
 		return ""
 	return (script as Script).get_global_name()
+
+
+## Log message at LogStream.LogLevel.INFO
+func log_info(message: String, values: Variant = null) -> void:
+	_logger.info(message, values)
+
+
+## Log message at LogStream.LogLevel.WARN
+func log_warning(message: String, values: Variant = null) -> void:
+	_logger.warn(message, values)
+
+
+## Log message at LogStream.LogLevel.ERROR
+func log_error(message: String, values: Variant = null) -> void:
+	_logger.err(message, values)
+
+
+## Log message at LogStream.LogLevel.DEBUG
+func log_debug(message: String, values: Variant = null) -> void:
+	_logger.dbg(message, values)
+
+
+## Log message at LogStream.LogLevel.FATAL
+func log_fatal(message: String, values: Variant = null) -> void:
+	_logger.fatal(message, values)
--- Ikaros/core/scene.gd
+++ Ikaros/core/scene.gd
@@ -5,24 +5,18 @@ extends IkarosNode
 
 var camera_controller: IkarosCameraController
 
-var _logger: LogStream
-
-
-func _init() -> void:
-	_logger = Ikaros.get_logger(get_class_name())
-
 
 func _ready() -> void:
 	var camera_controller_nodes: Array[Node] = find_children("", "IkarosCameraController")
 	camera_controller = camera_controller_nodes.front() as IkarosCameraController
 
 	if camera_controller == null:
-		_logger.error("Scene has no IkarosCameraController node.")
+		log_error("Scene has no IkarosCameraController node.")
 		Ikaros.quit()
 		return
 
 	if player == null:
-		_logger.warn("Player not defined in scene")
+		log_warning("Player not defined in scene")
 	else:
 		Ikaros.player = player
 		attach_camera_to_player()
@@ -30,13 +24,13 @@ func _ready() -> void:
 
 func attach_camera_to_player() -> void:
 	if player == null:
-		_logger.warn("Couldn't attach camera to player: Player not defined or null in scene.")
+		log_warning("Couldn't attach camera to player: Player not defined or null in scene.")
 		return
 
 	if player.is_queued_for_deletion():
-		_logger.warn("Couldn't attach camera to player: Player queued for free.")
+		log_warning("Couldn't attach camera to player: Player queued for free.")
 		return
 
-	_logger.info("Attaching camera to player.")
+	log_info("Attaching camera to player.")
 	player.add_child(camera_controller.camera_root)
 	camera_controller.camera_root.set_owner(player)
