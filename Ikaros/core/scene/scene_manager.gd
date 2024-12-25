9ee3a639ad2665306c7f4ccf7157c58179a4ffce
aronand <71503746+aronand@users.noreply.github.com>
2024-12-25 11:47:35 +0200

    feat: Adds default scene variable to scene manager

--- Ikaros/core/scene/scene_manager.gd
+++ Ikaros/core/scene/scene_manager.gd
@@ -1,6 +1,8 @@
 class_name IkarosSceneManager
 extends IkarosNode
 
+@export var default_scene: PackedScene = null
+
 
 func _init() -> void:
 	pass
@@ -9,6 +11,7 @@ func _init() -> void:
 func _ready() -> void:
 	assert(has_node("IkarosCameraController"))
 	assert(has_node("IkarosPlayerController"))
+	assert(default_scene != null)
 
 
 func _process(_delta: float) -> void:
